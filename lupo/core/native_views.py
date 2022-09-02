f40632be169c72880c872fd7d4dcd235ca393e7d
Anthony <anthonyhofmeister@gmx.de>
2022-09-02 14:25:01 +0200

    OnClick Argument (yet to be implemented) and basic Styling for Button

--- lupo/core/native_views.py
+++ lupo/core/native_views.py
@@ -1,4 +1,7 @@
+import builtins
+
 from .view import View
+from .style import Style
 import sys
 
 if sys.platform == "darwin":
@@ -8,14 +11,22 @@ if sys.platform == "darwin":
 
 class Button(View):
     text: str
+    onclick = None
 
-    def __init__(self, text: str):
-        super().__init__()
+    def __init__(self, text: str, style: Style=None, onclick=None):
+        super().__init__(style=style)
         self.text = text
+        self.onclick = onclick
 
     def get_osx_render(self, parent=None, superview: NSView = None):
         b = NSButton.alloc().initWithFrame_(((0, 0), (0, 0)))
         b.setBezelStyle_(4)
         b.setTitle_(self.text)
         b.sizeToFit()
+
+        btn_frame = b.frame()
+        btn_frame.size.width = self.style.width if self.style.width is not None else btn_frame.size.width
+        btn_frame.size.height = self.style.height if self.style.height is not None else btn_frame.size.height
+        b.setFrame_(btn_frame)
+
         return b
